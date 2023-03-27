abfb3f6a07034d2f27d4c1d7e82c0a39c63dfdcd
hedi-ghodhbane <52201138+hedi-ghodhbane@users.noreply.github.com>
2023-03-27 16:54:45 +0100

    add translation to buttons

--- vite-project/src/Counter.tsx
+++ vite-project/src/Counter.tsx
@@ -7,10 +7,17 @@ export const Counter = ()=>{
 
     return <div className='flex flex-col gap-4 items-center'>
         {formatMessage({id:'count'})}
-        <div className='bg-green-100 text-black text-3xl rounded-full font-bold w-20 h-20 items-center flex justify-center'>{count}</div>
+        <div className='bg-green-100 text-black
+        text-3xl rounded-full font-bold w-20 h-20 items-center flex justify-center'>
+            {count}
+        </div>
         <div className='flex gap-4'>
-        <button className='bg-white text-black py-2 px-4 rounded-full' onClick={()=>setCount(count+1)}>Increment</button>
-        <button className='bg-white text-black py-2 px-4 rounded-full' onClick={()=>setCount(count-1)}>Decrement</button>
+        <button className='bg-white text-black py-2 px-4 rounded-full'
+                onClick={()=>setCount(count+1)}>{formatMessage({id:'increase'})}
+        </button>
+        <button className='bg-white text-black py-2 px-4 rounded-full'
+                onClick={()=>setCount(count-1)}>{formatMessage({id:'decrease'})}
+        </button>
         </div>
     </div>
 }
